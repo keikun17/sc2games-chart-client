d6b1a5265e181c5fcf2aedecd7b1ce86397671ad
Buddy Magsipoc <keikun17@gmail.com>
2015-11-21 01:11:49 +0800

    Abstract games fetcher a bit。

--- source/javascripts/bundle.js
+++ source/javascripts/bundle.js
@@ -25919,17 +25919,41 @@
 	    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(App).call(this, props));
 	
 	    var date_pointer = new Date();
-	    var dates = [];
+	    var dates = {};
+	    var most_played = 0;
 	
+	    // Count games played per day, since last year
+	    // Record the most games played
 	    for (var i = 0; i < 365; i += 1) {
 	      var year = date_pointer.getFullYear();
 	      var month = date_pointer.getMonth() + 1;
 	      var day = date_pointer.getDate();
-	      dates.push([year, month, day].join('-'));
+	
+	      var date = [year, month, day].join('-');
+	
+	      // TODO : Replace with actual game count call
+	      // Generate game from 0 to 9
+	      var random_games_count = Math.floor(Math.random() * 10);
+	      var games = [];
+	
+	      for (var j = 0; j < random_games_count; j += 1) {
+	        games.push('Lost Temple 1v1 (Win)');
+	      }
+	
+	      dates[date] = { games: games };
+	
+	      var game_count = dates[date].games.length;
+	
+	      if (game_count > most_played) {
+	        most_played = game_count;
+	      }
+	
 	      date_pointer.setDate(date_pointer.getDate() - 1);
 	    }
 	
-	    _this.state = { dates: dates };
+	    _this.state = { dates: dates, most_played: most_played };
+	
+	    console.log(_this.state);
 	    return _this;
 	  }
 	
@@ -25940,9 +25964,37 @@
 	      var box_classnames = ['l1', 'l2', 'l3', 'w1', 'w2', 'w3', 'none'];
 	      var boxes = [];
 	
-	      for (var i = 0; i < 365; i++) {
+	      for (var date in this.state.dates) {
 	        var random_classname = box_classnames[Math.floor(Math.random() * box_classnames.length)];
-	        boxes.push(_react2.default.createElement('box', { className: random_classname, key: i + 1 }));
+	
+	        var games_played = this.state.dates[date].games.length;
+	        var classname;
+	
+	        var l1_max = this.state.most_played / 4;
+	        var l2_max = l1_max * 2;
+	        var l3_max = l1_max * 3;
+	
+	        if (1 <= games_played && games_played <= l1_max) {
+	          classname = 'l1';
+	        }
+	
+	        if (l1_max <= games_played && games_played <= l2_max) {
+	          classname = 'l2';
+	        }
+	
+	        if (l2_max <= games_played && games_played <= l3_max) {
+	          classname = 'l3';
+	        }
+	
+	        if (l3_max <= games_played && games_played <= this.state.most_played) {
+	          classname = 'l4';
+	        }
+	
+	        if (games_played === 0) {
+	          classname = 'none';
+	        }
+	
+	        boxes.push(_react2.default.createElement('box', { className: classname, key: date }));
 	      }
 	
 	      return _react2.default.createElement(
